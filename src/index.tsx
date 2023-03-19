d394161f58885fb8249618d1091f51de17d0dbe5
obidjonov1 <obidjonov@naver.com>
2023-03-19 20:13:49 +0900

    feat: prepare app for lazzatfood react project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './app/App';
+import reportWebVitals from './reportWebVitals';
+import './css/index.css';
 
 const container = document.getElementById('root')!;
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
@@ -11,3 +17,7 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
