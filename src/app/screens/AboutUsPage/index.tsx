55e5d09675c31018480ddb043b80c07b7477425c
obidjonov1 <obidjonov@naver.com>
2023-05-04 23:50:22 +0900

    feat: about us page modify

--- src/app/screens/AboutUsPage/index.tsx
+++ src/app/screens/AboutUsPage/index.tsx
@@ -1,6 +1,30 @@
 import { Container } from "@mui/system";
 import React from "react";
 
+
 export function AboutUsPage() {
-  return <Container>AboutUsPage</Container>;
+  return (
+    <Container>
+      <div className="about-us__title">
+        <h2>Assalomu Alaykum !</h2>
+        <p>Sahifamizga xush kelibsiz !</p>
+        <span>
+          Bizning asosiy maqsadimiz Koreyadagi hayotingizni yanada qiziqarli
+          qilishdir. Biz o'z oldimizga qo'ygan eng muhim maqsadimiz - sizga
+          sifatli mahsulot va xizmatlarni taklif qilishdir. Tashkil
+          etilganimizdan beri biz bir qator xizmatlarni, jumladan, mobil hisobni
+          to'ldirish va onlayn-do'konni ishga tushirdik. Sizning doimiy
+          qo'llab-quvvatlashingiz tufayli biz muhim tashkilotga aylandik. <br />
+          <br />
+          Bugungi kunda bizning xizmatlarimizdan har kuni o'n mingdan ortiq
+          mijozlar foydalanadi. Bundan tashqari, biz hamkorlarimizga o'z
+          minnatdorchiligimizni bildiramiz va kelgusida ham samarali va
+          muvaffaqiyatli hamkorlikni davom ettirishga umid qilamiz. Bizning
+          mahsulot va xizmatlarimizni tanlaganingiz uchun jamoamiz nomidan
+          minnatdorchilik bildiramiz. Sizning qo'llab-quvvatlashingiz biz uchun
+          juda muhim va sizga xizmat qilish bizning ustuvor vazifamizdir.
+        </span>
+      </div>
+    </Container>
+  );
 }
