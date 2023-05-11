adc7b9acabf88a1150b3ac2b1de566930965b422
obidjonov1 <obidjonov@naver.com>
2023-05-12 03:14:47 +0900

    feat: develop nested routing for market page

---
+++ src/app/screens/MarketPage/allMarkets.tsx
@@ -0,0 +1,6 @@
+import { Container } from "@mui/system";
+import React from "react";
+
+export function AllMarkets() {
+  return <Container>AllMarkets</Container>;
+}
---
+++ src/app/screens/MarketPage/chosenProduct.tsx
@@ -0,0 +1,6 @@
+import { Container } from "@mui/system";
+import React from "react";
+
+export function ChosenProduct() {
+  return <Container>ChosenProduct</Container>;
+}
